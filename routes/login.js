9c5d52fda4a0b2417adf25debefad0a6daff61bd
o0eason0o <ylu23@syr.edu>
2016-03-31 00:26:07 -0400

    pp get post

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,86 @@
 var router = require('express').Router();
 var db = require('mongodb').MongoClient.connect('mongodb://localhost/test');
 var Q = require('q');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
+
+passport.use('ppget', new LocalStrategy({
+        passReqToCallback: true
+    },
+    function(req, username, password, done) {
+        var info = {}
+        if (req.session && !!req.session.user && !!req.session.user.username)
+            info.username = req.session.user.username;
+        return done(null, info);
+    }
+));
+
+
+passport.use('pppost', new LocalStrategy({
+        passReqToCallback: true
+    },
+    function(req, username, password, done) {
+        var user = req.body;
+        console.log(user);
+    // TODO, check username and password
+        if (!user || !user.username || !user.password) {
+            return done(null, false, {
+                msg: 'username or password is needed'
+            });
+        }
+        db.then(function(db) {
+            // check user exists and valid
+            return db.collection('users').find(user).toArray()
+        }).then(function(users) {
+            if (!users || users.length == 0) {
+                return done(null, false, {
+                    msg: 'username and password not matched'
+                });
+            }
+            var deferred = Q.defer();
+            req.session.user = users[0];
+            req.session.save(function(err) {
+                if (err) deferred.reject(err)
+                else deferred.resolve({
+                    username: user.username
+                })
+            })
+            return deferred.promise;
+        }).then(function(info) {
+            return done(null, info)
+        }).catch(function(err) {
+            console.log('login fail', err);
+            next(err);
+        });
+    }
+));
 
 router.get('/', function(req, res, next) {
+    passport.authenticate('ppget', function(err, user, info) {
+        console.log(err, user, info);
+        if (err) {
+            return next(err);
+        }
+        if (!!info) {
+            return res.json(info);
+        }
+        else res.json(user);
+    })(req, res, next);
+});
+
+router.post('/', function(req, res, next){
+    passport.authenticate('pppost', function(err, user, info){
+        console.log(err, user, info);
+        if (!!info) {
+            res.json(info);
+        }
+        else res.json(user);
+    })(req, res, next);
+});
+
+/*router.get('/', function(req, res, next) {
     var info = {}
     if (req.session && !!req.session.user && !!req.session.user.username)
         info.username = req.session.user.username;
@@ -42,7 +120,7 @@ router.post('/', function(req, res, next) {
         console.log('login fail', err);
         next(err);
     });
-});
+}); */
 
 router.delete('/', function(req, res, next) {
     req.session.destroy(function(err) {
